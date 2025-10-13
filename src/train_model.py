Fix bug in standard deviation calculation

src/train_model.py
@@ -131,8 +131,8 @@ training_losses, validation_losses = run_epochs(
     optimizer = optimizer,
     model = model,
     alpha = 0.0,
-    eta0 = 1.0,
-    eta2 = 1.0,
+    eta0 = 1.0e-1,
+    eta2 = 1.0e-1,
     std = std,
     DEVICE = "cpu",
     scheduler = scheduler
@@ -215,6 +215,6 @@ plt.show()
 Aout = lambda w: spe_spectral(w, poles_mean, residues_mean)
 MSE_A = lambda w: np.square(Aout(w) - A(w))
 spectral_error, tol = integrate.quad(MSE_A, -np.inf, np.inf, epsabs=1e-10)
-print("\int_\infty^\infty dw |A_out(w) - A(w)|^2 = ", spectral_error)
+print("\int_{-\infty}^{\infty} dw |A_out(w) - A(w)|^2 = ", spectral_error)
 
 # %%

src/utilities/greens_stats.py
@@ -3,7 +3,7 @@ import numpy as np
 def calculate_cov(datafile):
     
     samples = np.loadtxt(datafile, delimiter=",")
-    samples = samples[:,1:] # trim tau = 0, beta points
+    samples = samples[:,0:-1] # trim tau = beta point
     Cov = np.cov(samples.T)
     Cov = Cov
     
@@ -13,7 +13,7 @@ def calculate_jackknife_cov(datafile):
     
     samples = np.loadtxt(datafile, delimiter=",")
     N = samples.shape[0]
-    samples = samples[:,1:] # trim tau = 0, beta points
+    samples = samples[:,0:-1] # trim tau = beta point
     sample_mean = np.mean(samples, axis=0)
     samples = (N*sample_mean - samples)/(N-1)
     Cov = np.cov(samples.T)
@@ -23,7 +23,7 @@ def calculate_jackknife_cov(datafile):
 def calculate_std(datafile, of_mean = True):
     
     samples = np.loadtxt(datafile, delimiter=",")
-    samples = samples[:,1:] # trim tau = 0, beta points
+    samples = samples[:,0:-1] # trim tau = beta point
     std = np.std(samples, axis=0, ddof = 1)
     if of_mean:
         std = std / np.sqrt(samples.shape[0])