e11fda3fe7d8a668b6e8e76738a09185dbee5a7d
Benjamin Cohen-Stead <benwcs@gmail.com>
2025-10-13 11:27:21 -0400

    Working Update

--- src/utilities/loss_functions.py
+++ src/utilities/loss_functions.py
@@ -19,11 +19,13 @@ def vae_loss(poles, residues, Gtau_out, Gtau_in, mu, logvar, alpha, eta0, eta2, 
     kl_divergence = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar_clamped.exp()) / batch_size
 
     # penalize the G(tau) curves going negative
-    positivity_error = torch.sum(F.relu(-Gtau_out / std) ** 2) / (Ltau * batch_size)
+    # positivity_error = torch.sum(F.relu(-Gtau_out / std) ** 2) / (Ltau * batch_size)
+    positivity_error = torch.sum(F.relu(-Gtau_out)) / batch_size
 
     # penalize the second derivative of G(tau) curve going negative
     dG2 = torch.diff(Gtau_out, n=2, dim=-1)
-    dG2_std = torch.sqrt(std[:, :-2] ** 2 + 4 * std[:, 1:-1] ** 2 + std[:, 2:] ** 2)
-    sfd_positivity_error = torch.sum(F.relu(-dG2 / dG2_std) ** 2) / ((Ltau - 2) * batch_size)
+    # dG2_std = torch.sqrt(std[:, :-2] ** 2 + 4 * std[:, 1:-1] ** 2 + std[:, 2:] ** 2)
+    # sfd_positivity_error = torch.sum(F.relu(-dG2 / dG2_std) ** 2) / ((Ltau - 2) * batch_size)
+    sfd_positivity_error = torch.sum(F.relu(-dG2)) / batch_size
 
     return mse_loss + alpha * kl_divergence + eta0 * positivity_error + eta2 * sfd_positivity_error
