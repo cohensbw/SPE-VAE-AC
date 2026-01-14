Normalization Fixes for Covariance MSE

src/train_model.py
@@ -82,8 +82,8 @@ A = lambda w: 0.6*stats.norm.pdf(w, loc = 2.0, scale = 1.0) + 0.4*stats.norm.pdf
 # inv_var_4 = torch.tensor(inv_var_4, dtype = dtype)
 
 # calculate covariance matrices
-W, var0, var2, var4 = calculate_cov_and_derivatives(datafile, variance_threshold = 0.99)
-W = torch.tensor(W, dtype = dtype)
+C, sqrt_C, inv_sqrt_C, n_components, var0, var2, var4 = calculate_cov_and_derivatives(datafile, variance_threshold = 0.999)
+inv_sqrt_C = torch.tensor(inv_sqrt_C, dtype = dtype)
 var0 = torch.tensor(var0, dtype = dtype)
 var2 = torch.tensor(var2, dtype = dtype)
 var4 = torch.tensor(var4, dtype = dtype)
@@ -109,41 +109,41 @@ testing_dataloader = DataLoader(dataset = testing_dataset, batch_size = batch_si
 # %%
 
 # initialize model
-model = VAE1(
-    beta = beta,
-    dtau = dtau,
-    num_poles = 8,
-    latent_dim = (4*8-2),
-    encoder_channels = [16, 32, 64],
-    encoder_kernel_sizes = [9, 9, 9],
-    encoder_strides = [2, 2, 2],
-    encoder_dilations = [1, 1, 1],
-    encoder_paddings = [4, 4, 4],
-    encoder_padding_mode = "reflect",
-    quadrature_nodes = 256,
-    matsubara_max = 256,
-    dtype = dtype
-)
-
-# model = VAE3(
+# model = VAE1(
 #     beta = beta,
 #     dtau = dtau,
 #     num_poles = 8,
 #     latent_dim = (4*8-2),
 #     encoder_channels = [16, 32, 64],
-#     encoder_kernel_sizes = [9, 9, 9],
+#     encoder_kernel_sizes = [11, 9, 7],
 #     encoder_strides = [2, 2, 2],
 #     encoder_dilations = [1, 1, 1],
+#     encoder_paddings = [5, 4, 3],
+#     encoder_padding_mode = "reflect",
 #     quadrature_nodes = 256,
 #     matsubara_max = 256,
 #     dtype = dtype
 # )
 
+model = VAE3(
+    beta = beta,
+    dtau = dtau,
+    num_poles = 10,
+    latent_dim = (4*10-2),
+    encoder_channels = [16, 32, 64],
+    encoder_kernel_sizes = [9, 9, 9],
+    encoder_strides = [2, 2, 2],
+    encoder_dilations = [1, 1, 1],
+    quadrature_nodes = 256,
+    matsubara_max = 256,
+    dtype = dtype
+)
+
 # %%
 
 # configure training procedure
-init_learning_rate = 3.0e-3
-final_learning_rate = 3.0e-4
+init_learning_rate = 1.0e-3
+final_learning_rate = 1.0e-4
 num_epochs = 100
 weight_decay = 0.0
 
@@ -173,11 +173,11 @@ else:
     validation_dataloader = testing_dataloader,
     optimizer = optimizer,
     model = model,
-    alpha = 0.00,
+    alpha = 1e-5,
     eta0 = 1.0,
     eta2 = 1.0,
-    eta4 = 0.0,
-    W = W,
+    eta4 = 1.0,
+    inv_sqrt_C = inv_sqrt_C,
     var0 = var0,
     var2 = var2,
     var4 = var4

src/utilities/greens_stats.py
@@ -50,60 +50,6 @@ def calculate_var_and_derivatives(datafile, eps=1e-6):
     
     return var, var2, var4
 
-# def calculate_cov_and_derivatives(datafile, variance_threshold=0.99):
-    
-#     # Load samples and remove last tau point
-#     samples = np.loadtxt(datafile, delimiter=",")
-#     samples = samples[:, :-1]  # remove tau=beta point
-#     N_samples, Ltau = samples.shape
-
-#     # --- Covariance of G(tau) ---
-#     C = np.cov(samples.T, bias=False)
-    
-#     # Whitening transformation for C with variance threshold
-#     w, V = np.linalg.eigh(C)
-#     w = np.maximum(w, 0.0)
-#     w_sorted = np.sort(w)[::-1]  # Sort in descending order
-#     cumsum = np.cumsum(w_sorted)
-#     total_var = cumsum[-1]
-#     n_components = np.searchsorted(cumsum, variance_threshold * total_var) + 1
-#     w_floor = w_sorted[n_components - 1]
-#     w = np.maximum(w, w_floor)
-#     W = V @ np.diag(1/np.sqrt(w)) * np.sqrt(n_components)
-#     C = V @ np.diag(w) @ V.T  # Reconstruct regularized covariance
-    
-#     # Trace of covariance
-#     TrC = np.trace(C)
-
-#     # --- Second derivative operator ---
-#     D2 = np.zeros((Ltau-2, Ltau))
-#     for i in range(Ltau-2):
-#         D2[i, i]   = 1.0
-#         D2[i, i+1] = -2.0
-#         D2[i, i+2] = 1.0
-
-#     # Covariance of second derivative
-#     C2 = D2 @ C @ D2.T
-    
-#     # Trace of second derivative covariance
-#     TrC2 = np.trace(C2)
-
-#     # --- Fourth derivative operator ---
-#     D4 = np.zeros((Ltau-4, Ltau))
-#     for i in range(Ltau-4):
-#         D4[i, i]   = 1.0
-#         D4[i, i+1] = -4.0
-#         D4[i, i+2] = 6.0
-#         D4[i, i+3] = -4.0
-#         D4[i, i+4] = 1.0
-
-#     # Covariance of fourth derivative
-#     C4 = D4 @ C @ D4.T
-    
-#     # Trace of fourth derivative covariance
-#     TrC4 = np.trace(C4)
-    
-#     return W, TrC, TrC2, TrC4
 
 def calculate_cov_and_derivatives(datafile, variance_threshold=0.99):
     
@@ -118,49 +64,26 @@ def calculate_cov_and_derivatives(datafile, variance_threshold=0.99):
     # Whitening transformation for C with variance threshold
     w, V = np.linalg.eigh(C)
     w = np.maximum(w, 0.0)
+    sqrt_C = V @ np.diag(np.sqrt(w)) @ V.T
+    sum_w = np.sum(w)
     idx = np.argsort(w)[::-1]  # Sort in descending order
     w_sorted = w[idx]
     V_sorted = V[:, idx]
     cumsum = np.cumsum(w_sorted)
     n_components = np.searchsorted(cumsum, variance_threshold * cumsum[-1]) + 1
     w_trunc = w_sorted[:n_components]
+    w_trunc = w_trunc * sum_w / np.sum(w_trunc)  # Rescale eigenvalues
     V_trunc = V_sorted[:, :n_components]
-    W = V_trunc @ np.diag(1/np.sqrt(w_trunc)) * np.sqrt(n_components)
-    C = V_trunc @ np.diag(w_trunc) @ V_trunc.T  # Reconstruct regularized covariance
+    eps = 1e-12
+    inv_sqrt_C = V_trunc @ np.diag(1/np.sqrt(w_trunc+eps)) @ V_trunc.T * np.sqrt(Ltau / n_components)
     
-    # variances
+    # regularized variance
     var0 = np.diag(C)
-
-    # # --- Second derivative operator ---
-    # D2 = np.zeros((Ltau-2, Ltau))
-    # for i in range(Ltau-2):
-    #     D2[i, i]   = 1.0
-    #     D2[i, i+1] = -2.0
-    #     D2[i, i+2] = 1.0
-
-    # # Covariance of second derivative
-    # C2 = D2 @ C @ D2.T
     
-    # # variances
-    # var2 = np.diag(C2)
-    
+    # regularized variance of second derivative
     var2 = var0[2:] + 4 * var0[1:-1] + var0[:-2]
-
-    # # --- Fourth derivative operator ---
-    # D4 = np.zeros((Ltau-4, Ltau))
-    # for i in range(Ltau-4):
-    #     D4[i, i]   = 1.0
-    #     D4[i, i+1] = -4.0
-    #     D4[i, i+2] = 6.0
-    #     D4[i, i+3] = -4.0
-    #     D4[i, i+4] = 1.0
-
-    # # Covariance of fourth derivative
-    # C4 = D4 @ C @ D4.T
     
-    # # variances
-    # var4 = np.diag(C4)
-    
+    # regularized variance of fourth derivative
     var4 = var0[4:] + 16 * var0[3:-1] + 36 * var0[2:-2] + 16 * var0[1:-3] + var0[:-4]
     
-    return W, var0, var2, var4
+    return C, sqrt_C, inv_sqrt_C, n_components,var0, var2, var4

src/utilities/loss_functions.py
@@ -42,17 +42,15 @@ def vae_loss(
     Gtau_out, Gtau_in,
     mu, logvar,
     alpha, eta0, eta2, eta4,
-    W, var0, var2, var4
+    inv_sqrt_C, var0, var2, var4
 ):
     
     batch_size, Ltau = Gtau_out.shape
 
     # calculate MSE loss
     dG = Gtau_out - Gtau_in
-    dG_white = dG @ W
+    dG_white = dG @ inv_sqrt_C
     mse_loss = torch.mean(torch.sum(dG_white**2, dim=1) / Ltau)
-    # mse_loss = torch.abs(1.4142135623730951 - mse_loss)
-    # mse_loss = torch.abs(1.0 - mse_loss)
     
     # KL divergence loss
     logvar_clamped = torch.clamp(logvar, min=-50, max=50)
@@ -62,16 +60,19 @@ def vae_loss(
     # G(tau) negativity loss
     neg0 = F.relu(-Gtau_out)
     negativity_loss_0 = eta0 * torch.mean(torch.sum(neg0**2/var0, dim=1) / Ltau)
+    # negativity_loss_0 = eta0 * torch.mean(torch.sum(neg0**2, dim=1)) / var0.mean() / Ltau
 
     # G''(tau) negativity loss
     dG2 = (Gtau_out[:, 2:] - 2 * Gtau_out[:, 1:-1] + Gtau_out[:, :-2])
     neg2 = F.relu(-dG2)
     negativity_loss_2 = eta2 * torch.mean(torch.sum(neg2**2/var2, dim=1) / (Ltau - 2))
+    # negativity_loss_2 = eta2 * torch.mean(torch.sum(neg2**2, dim=1)) / var2.mean() / (Ltau - 2)
 
     # G''''(tau) negativity loss
     dG4 = (Gtau_out[:, 4:] - 4 * Gtau_out[:, 3:-1] + 6 * Gtau_out[:, 2:-2] - 4 * Gtau_out[:, 1:-3] + Gtau_out[:, :-4])
     neg4 = F.relu(-dG4)
     negativity_loss_4 = eta4 * torch.mean(torch.sum(neg4**2/var4, dim=1) / (Ltau - 4))
+    # negativity_loss_4 = eta4 * torch.mean(torch.sum(neg4**2, dim=1)) / var4.mean() / (Ltau - 4)
 
     # total loss
     total_loss = (mse_loss + kl_loss + negativity_loss_0 + negativity_loss_2 + negativity_loss_4)